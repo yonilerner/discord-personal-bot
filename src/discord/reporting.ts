6823f62888164eb53a7944be50f34262a2fef1bc
Yoni Lerner <yonilerner@gmail.com>
2018-11-04 19:27:07 -0800

    Notifications 2 (#1)
    Adding notification scheduling

--- src/discord/reporting.ts
+++ src/discord/reporting.ts
@@ -2,11 +2,11 @@ import {client} from './client'
 import {globals} from '../config/globals'
 
 export async function report(message: string) {
-    let userToMessage = client.users.get(globals.USER_ID!)
-    if (!userToMessage) {
-        userToMessage = await client.fetchUser(globals.USER_ID!)
-    }
     try {
+        let userToMessage = client.users.get(globals.USER_ID)
+        if (!userToMessage) {
+            userToMessage = await client.fetchUser(globals.USER_ID)
+        }
         await userToMessage.send(message)
     } catch (e) {
         console.error('Unable to report message', message, e)
