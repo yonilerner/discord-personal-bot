fd8435c2c06e25245d0e378a8e232a8a4eac6972
Yoni Lerner <yonilerner@gmail.com>
2018-11-04 19:29:12 -0800

    Cleanup config

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,11 +1,6 @@
 const globals = {
     isDev: process.env.NODE_ENV === 'development',
 
-    DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_NAME,
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS,
-
     USER_ID: process.env.USER_ID,
     BOT_TOKEN: process.env.BOT_TOKEN,
 
