bf143f6f52c859a65e44fba769be232b9e0599f1
Yoni Lerner <yonilerner@gmail.com>
2018-11-19 01:31:07 +0000

    tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ npm run build
 
 forever stop yoni-bot
 rm $HOME/.forever/yoni-bot.log
-forever start --uid "yoni-bot" $HOME/node/discord-personal-bot/build/server.js
+forever start --uid "yoni-bot" build/server.js
